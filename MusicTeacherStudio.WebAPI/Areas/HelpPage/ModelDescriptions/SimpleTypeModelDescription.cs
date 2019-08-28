c72215c73d08e329ddb09f68aa9600d2f7e32355
drewawright <drewawright@gmail.com>
2019-08-28 12:15:04 -0400

    Add project files.

---
+++ MusicTeacherStudio.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MusicTeacherStudio.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MusicTeacherStudio.WebAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MusicTeacherStudio.WebAPI.WebApiApplication" Language="C#" %>
